a8bef261056eb692a7a9541492714b39f95194e0
chensj.dev <chensj881008@yeah.net>
2018-11-14 13:16:49 +0800

    去除DAO

--- src/main/java/com/winning/hic/dao/his/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * Created with IntelliJ IDEA.
- * Description: his 数据库接口表
- * User: LENOVO
- * Date: 2018-07-25
- * Time: 15:47
- */
-package com.winning.hic.dao.his;
--- src/main/java/com/winning/hic/dao/platform/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * Created with IntelliJ IDEA.
- * Description:平台数据库接口表
- * User: LENOVO
- * Date: 2018-07-25
- * Time: 15:48
- */
-package com.winning.hic.dao.platform;
