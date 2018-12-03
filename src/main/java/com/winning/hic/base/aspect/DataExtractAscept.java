fd90c3cf8b1c1b42887617460cd6d2de9e165c6e
Evol <798615946@qq.com>
2018-12-03 20:54:21 +0800

    抽取日子增加数据集过滤

--- src/main/java/com/winning/hic/base/aspect/DataExtractAscept.java
+++ src/main/java/com/winning/hic/base/aspect/DataExtractAscept.java
@@ -36,7 +36,8 @@ public class DataExtractAscept {
     private MbzOperateLogService mbzOperateLogService;
 
     @Pointcut("execution(* com.winning.hic.service.impl..interface*(..))")
-    public void servicePointcut(){}
+    public void servicePointcut() {
+    }
 
     //@Before("servicePointcut()")
     public void doBefore(JoinPoint joinPoint) {
@@ -53,7 +54,7 @@ public class DataExtractAscept {
         Object[] arguments = joinPoint.getArgs();
         MbzDataCheck entity = (MbzDataCheck) arguments[0];
         MbzOperateLog log = new MbzOperateLog();
-        log.setSetName(CisTableNameConfig.getDataListName(StringUtil.HumpToUnderline(methodName).replace("INTERFACE_","")));
+        log.setSetName(CisTableNameConfig.getDataListName(StringUtil.HumpToUnderline(methodName).replace("INTERFACE_", "")));
         log.setStartDate((String) entity.getMap().get("startDate"));
         log.setEndDate((String) entity.getMap().get("endDate"));
         //0:抽取中 1 :抽取完成
@@ -65,7 +66,13 @@ public class DataExtractAscept {
         long start = System.currentTimeMillis();
         try {
 
-            MbzDataCheck check = (MbzDataCheck)((ProceedingJoinPoint) joinPoint).proceed();
+            MbzDataCheck check = (MbzDataCheck) ((ProceedingJoinPoint) joinPoint).proceed();
+            if (check.getDataCount() == null || check.getRealCount() == null) {
+                MbzOperateLog mbzOperateLog = new MbzOperateLog();
+                mbzOperateLog.setId(log.getId());
+                mbzOperateLogService.removeMbzOperateLog(mbzOperateLog);
+                return;
+            }
             log.setUpdateTime(new Date());
             log.setStatus(1);
             log.setOperateCount(check.getRealCount());
